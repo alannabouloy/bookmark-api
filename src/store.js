10e35126e4a8972ab4519496e524ebf097e94548
Alanna Bouloy <lanihope@gmail.com>
2020-10-23 14:08:51 -0400

    test store removed

--- src/store.js
+++ src/store.js
@@ -1,11 +1,3 @@
-const bookmarks = [
-    {
-      "id": 1,
-      "title": "Google",
-      "url": "http://google.com",
-      "desc": "The best search engine",
-      "rating": 5
-    }
-];
+const bookmarks = [];
 
 module.exports = bookmarks;
